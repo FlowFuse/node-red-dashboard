25986dc4f1259a1f15fba475e06e70feaa901709
Joe Pavitt <joepavitt@flowforge.com>
2023-07-04 16:44:41 +0100

    Add "ui-markdown" widget

---
+++ docs/nodes/widgets/ui-markdown.md
@@ -0,0 +1,18 @@
+https://flowforge.com/handbook/development/markdown-how-to/#markdown-how-to
+
+---
+props:
+    Page: Defines with page of the UI Dashboard this widget will render on
+    Contents: The markdown to be passed to the UI and rendered
+---
+
+<script setup>
+</script>
+
+# Markdown Viewer `ui-markdown`
+
+Allows for markdown to be defined within Node-RED editor and rendered into the UI. Can be use for rendering labels, headers or even full blog articles.
+
+## Properties
+
+<PropsTable/>
---
+++ ui/src/widgets/ui-markdown/UIMarkdown.vue
@@ -0,0 +1,52 @@
+<template>
+    <vue-markdown class="nrdb-ui-markdown" :source="props.content" />
+</template>
+
+<script>
+    import { useDataTracker } from '../data-tracker.js'
+    import { mapState } from 'vuex'
+
+    import VueMarkdown from 'vue-markdown-render'
+
+    export default {
+        name: 'DBUIMarkdown',
+        inject: ['$socket'],
+        props: {
+            id: String,
+            props: Object
+        },
+        components: {
+            VueMarkdown
+        },
+        computed: {
+            ...mapState('data', ['values']),
+        },
+        created () {
+            useDataTracker(this.id)
+        }
+    }
+</script>
+  
+<style lang="css">
+.nrdb-ui-markdown h1 {
+    margin: 0.67em 0;
+}
+.nrdb-ui-markdown h2 {
+    margin: 0.5em 0;
+}
+.nrdb-ui-markdown h3 {
+    margin: 0.25em 0;
+}
+.nrdb-ui-markdown ul {
+    padding: 0 0 0 1em;
+}
+.nrdb-ui-markdown p {
+    margin: 0.25em 0 1em;
+}
+.nrdb-ui-markdown blockquote {
+    padding-left: 1em;
+    border-left: 4px solid #d1d1d1;
+    color: gray;
+}
+</style>
+  
