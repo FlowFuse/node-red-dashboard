bd64a910a5fc4e42cc35212e30ec5a0da04223ce
Joe Pavitt <joepavitt@flowforge.com>
2023-08-02 15:10:23 +0100

    Parking broken ui-template work

--- ui/src/widgets/ui-template/UITemplate.vue
+++ ui/src/widgets/ui-template/UITemplate.vue
@@ -1,10 +1,22 @@
 <template>
-    <div class="nrdb-ui-widget">Template Node: {{ props.format }}</div>
+    <div class="nrdb-ui-widget">
+        Using v-html= -
+        <div v-html="props.format" />
+        {{ props.format }}
+        Using component :is -
+        <component :is="template" />
+    </div>
 </template>
 
 <script>
+
+// eslint-disable vue/one-component-per-file
+
+import { defineComponent, markRaw } from 'vue'
+
 import { useDataTracker } from '../data-tracker.js' // eslint-disable-line import/order
 import { mapState } from 'vuex' // eslint-disable-line import/order
+// import UIButton from '../ui-button/UIButton.vue' // eslint-disable-line import/order
 
 export default {
     name: 'DBUITemplate',
@@ -15,6 +27,34 @@ export default {
     },
     setup (props) {
         useDataTracker(props.id)
+    },
+    data () {
+        console.log('data')
+        // eslint-disable-next-line vue/one-component-per-file
+        const options = markRaw(defineComponent({
+            template: '<div>Template Component</div>'
+        }))
+
+        console.log(options)
+
+        // const template = defineAsyncComponent(() => {
+        //     console.log('inside async')
+        //     return new Promise((resolve, reject) => {
+        //         console.log(UIButton)
+        //         // ...load component from server
+        //         resolve({
+        //             name: 'UITemplate',
+        //             render () {
+        //                 return h({ render: compile('<div>Template Component</div>') })
+        //             }
+        //         })
+        //     })
+        // })
+        // console.log(template)
+        // const template = defineAsyncComponent(() => import('../ui-button/UIButton.vue'))
+        return {
+            template: options
+        }
     },
     computed: {
         ...mapState('data', ['values'])
