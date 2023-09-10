4bd84da1ff88851eee9499ffde1e044fbada195c
Steve-Mcl <sdmclaughlin@gmail.com>
2023-09-10 14:37:35 +0100

    add mermaid support

--- ui/src/widgets/ui-markdown/UIMarkdown.vue
+++ ui/src/widgets/ui-markdown/UIMarkdown.vue
@@ -3,20 +3,46 @@ import DOMPurify from 'dompurify'
 import hljs from 'highlight.js'
 import 'highlight.js/styles/default.css'
 import { marked } from 'marked'
-import { markedHighlight } from 'marked-highlight'
-
+import mermaid from 'mermaid'
 import { h } from 'vue'
 import { mapGetters, mapState } from 'vuex' // eslint-disable-line import/order
 
 import { useDataTracker } from '../data-tracker.js'
 
-marked.use(markedHighlight({
-    langPrefix: 'hljs language-',
-    highlight (code, lang) {
+import { escapeHTML } from './../../util.js'
+
+// set up mermaid
+/** @type {import('mermaid').MermaidConfig} */
+const config = {
+    securityLevel: 'loose',
+    startOnLoad: false,
+    class: 'mermaid',
+    // theme: this.theme, // TODO: add theme support
+    flowchart: {
+        curve: 'linear'
+    },
+    darkMode: false // TODO: add dark mode support
+}
+mermaid.initialize(config)
+
+const customRenderer = {
+    name: 'code',
+    renderer (token) {
+        const { lang, text } = token
+        // wrap mermaid in a pre tag with class "mermaid" for the mermaid renderer to pick up
+        if (lang === 'mermaid') {
+            return `<pre class="mermaid">${text}</pre>`
+        }
+        // wrap all other code in a pre tag with hljs language-<lang> class for hljs to pick up
         const language = hljs.getLanguage(lang) ? lang : 'plaintext'
-        return hljs.highlight(code, { language }).value
+        const classAttr = ` class="hljs language-${escapeHTML(language)}"`
+        const sanitisedText = text.replace(/\n$/, '')
+        const code = hljs.highlight(sanitisedText, { language }).value
+        return `<pre><code class="hljs language-${classAttr}">${code}\n</code></pre>`
     }
-}))
+}
+
+marked.use({ extensions: [customRenderer] })
 
 export default {
     name: 'DBUIMarkdown',
@@ -48,9 +74,18 @@ export default {
                     return this.messages[this.id] || {}
                 }
             },
+            mounted () {
+                this.renderMermaid()
+            },
             methods: {
                 get (path, defaultValue) {
                     return this.getMsgProperty(this.id, path, defaultValue)
+                },
+                renderMermaid () {
+                    mermaid.run({
+                        querySelector: '.mermaid',
+                        suppressErrors: true
+                    })
                 }
             }
         }, {
