b828a3487b4d25c108462e77dd3366599a232b1c
Joe Pavitt <joepavitt@flowforge.com>
2023-10-18 11:55:46 +0100

    Fix socket connection cleanup & re-establish in constructor

--- nodes/config/ui_theme.js
+++ nodes/config/ui_theme.js
@@ -4,13 +4,12 @@ module.exports = function (RED) {
      * @param {*} config
      */
     function UIThemeNode (config) {
+        RED.nodes.createNode(this, config)
         const node = this
 
         // eslint-disable-next-line no-unused-vars
         const { id, name, type, _users, ...rest } = config
         node.colors = { ...rest.colors }
-
-        RED.nodes.createNode(node, config)
     }
     RED.nodes.registerType('ui-theme', UIThemeNode)
 }
