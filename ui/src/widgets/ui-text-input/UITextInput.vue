87f5df6494eb5602bcf626d309c3135eca91bf6e
Joe Pavitt <joepavitt@flowforge.com>
2023-11-22 16:17:07 +0000

    UI Text Input: onEnter, onDelay and onBlur controls

--- ui/src/widgets/ui-text-input/UITextInput.vue
+++ ui/src/widgets/ui-text-input/UITextInput.vue
@@ -2,12 +2,12 @@
     <v-text-field
         v-if="type !== 'textarea'"
         v-model="value" class="nrdb-ui-text-field"
-        :label="label" :type="type" :rules="validation" variant="outlined" hide-details="auto" @blur="onBlur"
+        :label="label" :type="type" :rules="validation" variant="outlined" hide-details="auto" @update:model-value="onChange" @keyup.enter="onEnter" @blur="onBlur"
     />
     <v-textarea
         v-else
         v-model="value" class="nrdb-ui-text-field"
-        :label="label" variant="outlined" hide-details="auto" @blur="onBlur"
+        :label="label" variant="outlined" hide-details="auto" @update:model-value="onChange" @blur="send"
     />
 </template>
 
@@ -25,6 +25,12 @@ export default {
     },
     setup (props) {
         useDataTracker(props.id)
+    },
+    data () {
+        return {
+            lastSent: null,
+            delayTimer: null
+        }
     },
     computed: {
         ...mapState('data', ['messages']),
@@ -56,8 +62,31 @@ export default {
         }
     },
     methods: {
-        onBlur: function () {
+        send: function () {
             this.$socket.emit('widget-change', this.id, this.value)
+            this.lastSent = this.value
+        },
+        onChange: function () {
+            if (this.props.sendOnDelay) {
+                // is send on delay enabled, if so, set a timeout to send the message
+                if (this.delayTimer) {
+                    // reset the timer to count from the latest change
+                    clearTimeout(this.delayTimer)
+                }
+                this.delayTimer = setTimeout(this.send, this.props.delay)
+            }
+        },
+        onBlur: function () {
+            if (this.props.sendOnBlur && this.lastSent !== this.value) {
+                // check if this value has already been sent, as not going to want it sent twice
+                this.send()
+            }
+        },
+        onEnter: function () {
+            if (this.props.sendOnEnter) {
+                // don't compare previous value, if user has pressed <enter> they want it submitted
+                this.send()
+            }
         }
     }
 }
