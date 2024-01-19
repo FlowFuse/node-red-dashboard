cd68d933dacc8c3dead55d79610d01140208c024
Joe Pavitt <joepavitt@flowforge.com>
2024-01-19 10:25:21 +0000

    Add disabled/enabled state for UI Radio Group

--- cypress/tests/behaviours/disable-widgets.spec.js
+++ cypress/tests/behaviours/disable-widgets.spec.js
@@ -31,32 +31,6 @@ describe('Node-RED Dashboard 2.0 - Can enable/disable widgets with msg.enabled f
         cy.get('#nrdb-ui-widget-dashboard-ui-switch-enabled-test').find('.v-input').should('have.class', 'v-input--disabled')
     })
 
-    it.skip('UI Chart', () => {
-        // Emitting strings
-        cy.get('button').contains('Send Disabled (ui-chart)').click()
-
-        // check in our Dashboard that the class has been applied
-        cy.get('#nrdb-ui-widget-0199063df3611ec6').should('have.class', 'test-class')
-
-        cy.reloadDashboard()
-
-        // ensure state is still there and data has persisted
-        cy.get('#nrdb-ui-widget-0199063df3611ec6').should('have.class', 'test-class')
-    })
-
-    it.skip('UI Text', () => {
-        // Emitting strings
-        cy.get('button').contains('Send Disabled (ui-text)').click()
-
-        // check in our Dashboard that the class has been applied
-        cy.get('#nrdb-ui-widget-1ae7b1574d0c8d63').should('have.class', 'test-class')
-
-        cy.reloadDashboard()
-
-        // ensure state is still there and data has persisted
-        cy.get('#nrdb-ui-widget-1ae7b1574d0c8d63').should('have.class', 'test-class')
-    })
-
     it('UI Text Input', () => {
         // Emitting strings
         cy.get('button').contains('Send Disabled (ui-text-input)').click()
@@ -68,32 +42,6 @@ describe('Node-RED Dashboard 2.0 - Can enable/disable widgets with msg.enabled f
 
         // ensure state is still there and data has persisted
         cy.get('#nrdb-ui-widget-dashboard-ui-text-input-enabled-test').find('.v-input').should('have.class', 'v-input--disabled')
-    })
-
-    it.skip('UI Table', () => {
-        // Emitting strings
-        cy.get('button').contains('Send Disabled (ui-table)').click()
-
-        // check in our Dashboard that the class has been applied
-        cy.get('#nrdb-ui-widget-47c8ab23d9b81c61').should('have.class', 'test-class')
-
-        cy.reloadDashboard()
-
-        // ensure state is still there and data has persisted
-        cy.get('#nrdb-ui-widget-47c8ab23d9b81c61').should('have.class', 'test-class')
-    })
-
-    it.skip('UI Markdown', () => {
-        // Emitting strings
-        cy.get('button').contains('Send Disabled (ui-markdown)').click()
-
-        // check in our Dashboard that the class has been applied
-        cy.get('#nrdb-ui-widget-13d50f316ddcd8e1').should('have.class', 'test-class')
-
-        cy.reloadDashboard()
-
-        // ensure state is still there and data has persisted
-        cy.get('#nrdb-ui-widget-13d50f316ddcd8e1').should('have.class', 'test-class')
     })
 
     it('UI Slider', () => {
@@ -122,16 +70,16 @@ describe('Node-RED Dashboard 2.0 - Can enable/disable widgets with msg.enabled f
         cy.get('#nrdb-ui-widget-dashboard-ui-dropdown-enabled-test').find('.v-input').should('have.class', 'v-input--disabled')
     })
 
-    it.skip('UI Radio Group', () => {
+    it('UI Radio Group', () => {
         // Emitting strings
         cy.get('button').contains('Send Disabled (ui-radio-group)').click()
 
         // check in our Dashboard that the class has been applied
-        cy.get('#nrdb-ui-widget-dce954ccc4b2d3df').should('have.class', 'test-class')
+        cy.get('#nrdb-ui-widget-dashboard-ui-radio-group-enabled-test').find('.v-input').should('have.class', 'v-input--disabled')
 
         cy.reloadDashboard()
 
         // ensure state is still there and data has persisted
-        cy.get('#nrdb-ui-widget-dce954ccc4b2d3df').should('have.class', 'test-class')
+        cy.get('#nrdb-ui-widget-dashboard-ui-radio-group-enabled-test').find('.v-input').should('have.class', 'v-input--disabled')
     })
 })
