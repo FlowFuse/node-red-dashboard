4833acda78e0b54a82799b2e1699e463f335efba
Joe Pavitt <joepavitt@flowforge.com>
2024-03-01 15:28:46 +0000

    UI Table - Switch to v-data-table to provide basic interaction, sorting, improved pagination, etc.

--- nodes/widgets/ui_table.js
+++ nodes/widgets/ui_table.js
@@ -8,6 +8,18 @@ module.exports = function (RED) {
         // which group are we rendering this widget
         const group = RED.nodes.getNode(config.group)
 
+        config.maxrows = parseInt(config.maxrows) || 0
+
+        if (config.columns) {
+            config.columns.map((col) => {
+                // map older data where 'label' was used.
+                return {
+                    title: col.title || col.label,
+                    key: col.key
+                }
+            })
+        }
+
         // inform the dashboard UI that we are adding this node
         group.register(node, config)
     }
