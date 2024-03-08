32e8d6825b20e88d409f64db3dbbea5b1fbb9e30
Joe Pavitt <joepavitt@flowforge.com>
2024-03-08 16:13:09 +0000

    Docs: First pass at subflows documentation

---
+++ docs/components/ComingSoon.vue
@@ -0,0 +1,26 @@
+<template>
+    <span class="coming-soon">Coming Soon</span>
+</template>
+
+<script>
+export default {
+    name: 'ComingSoon'
+}
+</script>
+
+<style scoped>
+.coming-soon {
+    display: inline-flex;
+    font-size: 14px;
+    line-height: 20px;
+    padding: 3px 9px;
+    color: var(--vp-c-brand-1);
+    margin-left: 0.5em;
+    border-radius: 2rem;
+    background-color: var(--vp-c-brand-soft);
+    border: 1px solid var(--vp-c-brand-1);
+    text-decoration: none;
+    transition: none;
+    vertical-align: middle;
+}
+</style>
---
+++ docs/components/NodeRedVersion.vue
@@ -0,0 +1,48 @@
+<template>
+    <a class="added-in node-red-version" :href="'https://github.com/node-red/node-red/releases/tag/v' + version" target="_blank">Node-RED v{{ version }}+</a>
+</template>
+
+<script>
+export default {
+    name: 'NodeRedVersion',
+    props: {
+        version: {
+            type: String,
+            required: true
+        }
+    }
+}
+</script>
+
+<style scoped>
+.added-in {
+    display: inline-flex;
+    font-size: 14px;
+    line-height: 20px;
+    padding: 3px 9px;
+    color: var(--vp-c-brand-1);
+    margin-left: 0.5em;
+    border-radius: 2rem;
+    background-color: var(--vp-c-brand-soft);
+    border: 1px solid var(--vp-c-brand-1);
+    text-decoration: none;
+    transition: none;
+    vertical-align: middle;
+}
+
+.added-in:hover {
+    background-color: var(--vp-c-brand-1);
+    color: #fff;
+}
+
+.added-in.node-red-version {
+    background-color: var(--vp-c-node-red);
+    color: var(--vp-c-node-red-dark);
+    border: 1px solid var(--vp-c-node-red-dark);
+}
+
+.added-in.node-red-version:hover {
+    background-color: var(--vp-c-node-red-dark);
+    color: #fff;
+}
+</style>
