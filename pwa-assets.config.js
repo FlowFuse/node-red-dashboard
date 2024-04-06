a12256f0ec719df3a659fd22e1406809a9d2a356
cgjgh <160297365+cgjgh@users.noreply.github.com>
2024-04-05 21:30:55 -0500

    Vite-PWA Plugin Dependency Fix

--- pwa-assets.config.js
+++ pwa-assets.config.js
@@ -1,12 +1,25 @@
 import {
-    defineConfig,
-    minimal2023Preset as preset
+    defineConfig
 } from '@vite-pwa/assets-generator/dist/config'
 
 export default defineConfig({
     headLinkOptions: {
         preset: '2023'
     },
-    preset,
+    preset: {
+        transparent: {
+            sizes: [64, 192, 512],
+            favicons: [[48, 'favicon.ico']],
+            padding: 0.1
+        },
+        maskable: {
+            sizes: [512],
+            padding: 0.1
+        },
+        apple: {
+            sizes: [180],
+            padding: 0.15
+        }
+    },
     images: ['./ui/public/logo-512x512.png']
 })
