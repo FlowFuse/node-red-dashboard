680ac60b0d878da558e6c2279c4bdc737b1db40f
bartbutenaers <bart.butenaers@gmail.com>
2024-04-10 22:21:58 +0100

    revert change evt

--- nodes/widgets/ui_notification.js
+++ nodes/widgets/ui_notification.js
@@ -7,12 +7,8 @@ module.exports = function (RED) {
         // which group are we rendering this widget
         const ui = RED.nodes.getNode(config.ui)
 
-        const evts = {
-            onChange: true
-        }
-
         // inform the dashboard UI that we are adding this node
-        ui.register(null, null, node, config, evts)
+        ui.register(null, null, node, config)
     }
     RED.nodes.registerType('ui-notification', NotificationNode)
 }
