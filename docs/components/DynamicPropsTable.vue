11edacac5c3dcdd1255d2608c743efa03f8feefd
Joe Pavitt <joepavitt@flowforge.com>
2024-05-15 17:23:30 +0100

    Docs - Improve rendering of "Dynamic Properties" table

--- docs/components/DynamicPropsTable.vue
+++ docs/components/DynamicPropsTable.vue
@@ -13,7 +13,12 @@
             <tr v-for="(value, property) in page.frontmatter?.dynamic" :key="property">
                 <td>{{ property }}</td>
                 <td><code>{{ value.payload }}</code></td>
-                <td><ul><li v-for="s in value.structure" :key="s"><code>{{ s }}</code></li></ul></td>
+                <td>
+                    <ul v-if="value.structure?.length > 1">
+                        <li v-for="s in value.structure" :key="s"><code>{{ s }}</code></li>
+                    </ul>
+                    <code v-else>{{ value.structure[0] }}</code>
+                </td>
             </tr>
         </tbody>
     </table>
