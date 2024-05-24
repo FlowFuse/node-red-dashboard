1e11faa3aeaba843ce826ce5da0da7f86803b174
cstns <cstn.serban@gmail.com>
2024-05-24 22:04:17 +0300

    add videos to cypress tests

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,8 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+    video: true,
+    videoCompression: true,
     e2e: {
         baseUrl: 'http://localhost:1881',
         specPattern: 'cypress/tests/**/*.spec.{js,jsx,ts,tsx}',
