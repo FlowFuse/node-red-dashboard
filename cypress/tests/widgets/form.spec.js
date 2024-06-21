bd9ce460d649e342a2f72184301b89eca0af14fc
Joe Pavitt <joepavitt@flowforge.com>
2024-06-21 17:10:02 +0100

    UI Form: Add support for dynamic options and label

--- cypress/tests/widgets/form.spec.js
+++ cypress/tests/widgets/form.spec.js
@@ -33,3 +33,41 @@ describe('Node-RED Dashboard 2.0 - Forms', () => {
         cy.get('[data-action="form-submit"]').should('not.be.disabled')
     })
 })
+
+describe.only('Node-RED Dashboard 2.0 - Forms', () => {
+    beforeEach(() => {
+        cy.deployFixture('dashboard-forms')
+        cy.visit('/dashboard/page1')
+    })
+
+    it('permits users to set default values via msg.payload', () => {
+        // check that the form is empty
+        cy.get('#nrdb-ui-widget-dashboard-ui-form-dynamic').find('[data-form="form-row-name0"]').find('input[type="text"]').should('have.value', '')
+        cy.clickAndWait(cy.get('button').contains('Set Defaults'))
+        cy.get('#nrdb-ui-widget-dashboard-ui-form-dynamic').find('[data-form="form-row-name0"]').find('input[type="text"]').should('have.value', 'Overridden Default Name')
+    })
+
+    it('can have their content defined by msg.ui_update.options', () => {
+        cy.get('#nrdb-ui-widget-dashboard-ui-form-dynamic').find('[data-form="form-row-name"]').should('not.exist')
+        cy.get('#nrdb-ui-widget-dashboard-ui-form-dynamic').find('[data-form="form-row-multiline"]').should('not.exist')
+        cy.get('#nrdb-ui-widget-dashboard-ui-form-dynamic').find('[data-form="form-row-password"]').should('not.exist')
+        cy.get('#nrdb-ui-widget-dashboard-ui-form-dynamic').find('[data-form="form-row-email"]').should('not.exist')
+        cy.get('#nrdb-ui-widget-dashboard-ui-form-dynamic').find('[data-form="form-row-age"]').should('not.exist')
+        cy.get('#nrdb-ui-widget-dashboard-ui-form-dynamic').find('[data-form="form-row-newsletter"]').should('not.exist')
+        cy.get('#nrdb-ui-widget-dashboard-ui-form-dynamic').find('[data-form="form-row-notifications"]').should('not.exist')
+        cy.get('#nrdb-ui-widget-dashboard-ui-form-dynamic').find('[data-form="form-row-dob"]').should('not.exist')
+        cy.get('#nrdb-ui-widget-dashboard-ui-form-dynamic').find('[data-form="form-row-tob"]').should('not.exist')
+
+        cy.clickAndWait(cy.get('button').contains('Override Form Options'))
+
+        cy.get('#nrdb-ui-widget-dashboard-ui-form-dynamic').find('[data-form="form-row-name"]').find('input[type="text"]').should('exist')
+        cy.get('#nrdb-ui-widget-dashboard-ui-form-dynamic').find('[data-form="form-row-multiline"]').find('textarea').should('exist')
+        cy.get('#nrdb-ui-widget-dashboard-ui-form-dynamic').find('[data-form="form-row-password"]').find('input[type="password"]').should('exist')
+        cy.get('#nrdb-ui-widget-dashboard-ui-form-dynamic').find('[data-form="form-row-email"]').find('input[type="email"]').should('exist')
+        cy.get('#nrdb-ui-widget-dashboard-ui-form-dynamic').find('[data-form="form-row-age"]').find('input[type="number"]').should('exist')
+        cy.get('#nrdb-ui-widget-dashboard-ui-form-dynamic').find('[data-form="form-row-newsletter"]').find('input[type="checkbox"]').should('exist')
+        cy.get('#nrdb-ui-widget-dashboard-ui-form-dynamic').find('[data-form="form-row-notifications"]').find('input[type="checkbox"]').should('exist')
+        cy.get('#nrdb-ui-widget-dashboard-ui-form-dynamic').find('[data-form="form-row-dob"]').find('input[type="date"]').should('exist')
+        cy.get('#nrdb-ui-widget-dashboard-ui-form-dynamic').find('[data-form="form-row-tob"]').find('input[type="time"]').should('exist')
+    })
+})
--- nodes/widgets/ui_form.js
+++ nodes/widgets/ui_form.js
@@ -1,3 +1,5 @@
+const statestore = require('../store/state.js')
+
 module.exports = function (RED) {
     function FormNode (config) {
         RED.nodes.createNode(this, config)
@@ -8,7 +10,19 @@ module.exports = function (RED) {
         if (!group) { return }
 
         const evts = {
-            onAction: true
+            onAction: true,
+            beforeSend: function (msg) {
+                if (msg.ui_update) {
+                    const update = msg.ui_update
+                    if (typeof update.options !== 'undefined') {
+                        // todo: sanity check options is valid format?
+
+                        // dynamically set "options" property
+                        statestore.set(group.getBase(), node, msg, 'options', update.options)
+                    }
+                }
+                return msg
+            }
         }
 
         // inform the dashboard UI that we are adding this node
