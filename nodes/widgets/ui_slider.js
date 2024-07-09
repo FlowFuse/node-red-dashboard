c9c9f16c447d3ca8366f8c03c0beb88a97dc8e92
Joe Pavitt <joepavitt@flowforge.com>
2024-07-09 17:22:58 +0100

    Remove icon click dynamic property check

--- nodes/widgets/ui_slider.js
+++ nodes/widgets/ui_slider.js
@@ -70,9 +70,6 @@ module.exports = function (RED) {
                     if (typeof (updates.iconAppend) !== 'undefined') {
                         statestore.set(group.getBase(), node, msg, 'iconAppend', updates.iconAppend)
                     }
-                    if (typeof (updates.iconClick) !== 'undefined') {
-                        statestore.set(group.getBase(), node, msg, 'iconClick', updates.iconClick)
-                    }
                     if (typeof (updates.color) !== 'undefined') {
                         statestore.set(group.getBase(), node, msg, 'color', updates.color)
                     }
