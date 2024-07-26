3b0ed3740da51d6af731e51b880cd58a52357acb
Gayan Sandamal <gayansandamal94@gmail.com>
2024-07-26 15:37:25 +0530

    Interactive dashboard implementation

---
+++ docs/components/TryDemo.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="try-out-wrapper">
+        <slot></slot>
+        <a :href="`https://dashboard-demos.flowfuse.cloud/dashboard/${href}`" target="_blank" class="--btn">
+            {{ title }}
+        </a>
+    </div>
+</template>
+
+<script setup>
+const { href, title } = defineProps({
+    href: {
+        type: String,
+        required: true
+    },
+    title: {
+        type: String,
+        default: 'Try Demo'
+    }
+})
+</script>
+
+<style scoped lang="scss">
+.try-out-wrapper {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    .--btn {
+        background-color: var(--vp-c-brand-3);
+        ;
+        color: white;
+        padding: 0.5rem 1rem;
+        border-radius: 0.25rem;
+        text-decoration: none;
+
+        &:hover {
+            background-color: var(--vp-c-brand-2);
+            cursor: pointer;
+        }
+    }
+}
+</style>
