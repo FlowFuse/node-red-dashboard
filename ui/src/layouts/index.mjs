7cb2da81414a2266b24f36dc8c34bac6aa073f05
Joe Pavitt <joepavitt@flowforge.com>
2024-07-28 17:34:38 +0100

    Layout: Tabs

--- ui/src/layouts/index.mjs
+++ ui/src/layouts/index.mjs
@@ -1,9 +1,11 @@
 import Flex from './Flex.vue'
 import Grid from './Grid.vue'
 import Notebook from './Notebook.vue'
+import Tabs from './Tabs.vue'
 
 export default {
     flex: Flex,
     grid: Grid,
+    tabs: Tabs,
     notebook: Notebook
 }
