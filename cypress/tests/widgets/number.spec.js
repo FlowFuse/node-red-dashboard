e25952bfdc6277fb95d8e923f323cdbf8bba4883
Gayan Sandamal <gayansandamal94@gmail.com>
2024-08-09 23:47:47 +0530

    Used cypress tests updated and reverted the cypress update

--- cypress/tests/widgets/number.spec.js
+++ cypress/tests/widgets/number.spec.js
@@ -1,11 +1,11 @@
 /* eslint-disable cypress/no-unnecessary-waiting */
-describe('Node-RED Dashboard 2.0 - Number Input Widget', () => {
+describe('Node/-RED Dashboard 2.0 - Number Input Widget', () => {
     beforeEach(() => {
         cy.deployFixture('dashboard-number-input')
         cy.visit('/dashboard/page1')
     })
 
-    const numberInputOne = '.number-input-1 input[type="text"]'
+    const numberInputOne = '#nrdb-ui-widget-5b751bff9a6c7439 input[type="text"]'
 
     // Test case: Renders the Number Input widget correctly
     it('renders the Number Input widget correctly', () => {
@@ -23,7 +23,7 @@ describe('Node-RED Dashboard 2.0 - Number Input Widget', () => {
 
     // Test case: Displays the tooltip correctly
     it('displays the tooltip correctly', () => {
-        cy.get('.number-input-1 .nrdb-ui-number-field').trigger('mouseover')
+        cy.get('#nrdb-ui-widget-5b751bff9a6c7439 .nrdb-ui-number-field').trigger('mouseover')
         cy.get('.v-tooltip').should('contain', 'Tooltip Text')
     })
 
@@ -31,44 +31,34 @@ describe('Node-RED Dashboard 2.0 - Number Input Widget', () => {
     it('reset field on onClear event correctly and outputs the correct payload', () => {
         cy.get(numberInputOne).clear()
         cy.get(numberInputOne).type('2')
-        cy.get('.number-input-1 .nrdb-ui-number-field .v-field__clearable').click()
+        cy.get('#nrdb-ui-widget-5b751bff9a6c7439 .nrdb-ui-number-field .v-field__clearable').click()
         cy.get(numberInputOne).should('have.value', '')
         cy.checkOutput('msg.payload', null)
-        cy.get('.number-input-1 .nrdb-ui-number-field').trigger('blur')
+        cy.get('#nrdb-ui-widget-5b751bff9a6c7439 .nrdb-ui-number-field').trigger('blur')
     })
 })
 
 describe('Node-RED Dashboard 2.0 - Number Input (Dynamic Properties)', () => {
-    const buttonSetIcon = '.set-icon-button button'
-    const buttonSetIconPosition = '.set-icon-position button'
-    const buttonSetIconOutside = '.set-icon-to-outside button'
-
     beforeEach(() => {
         cy.deployFixture('dashboard-number-input')
         cy.visit('/dashboard/page1')
     })
 
     it('Set the dynnamic properties: set input "icon"', () => {
-        cy.get(buttonSetIcon).trigger('click')
-        cy.wait(100)
-        cy.get('.nrdb-ui-number-input.number-input-2 .v-field i.mdi-numeric').should('exist')
+        cy.get('#nrdb-ui-widget-3616bf2d05dc6b68').should('exist')
+        cy.clickAndWait(cy.get('button').contains('Dynamic Property: Icon'))
+        cy.get('#nrdb-ui-widget-3616bf2d05dc6b68 .v-field__prepend-inner').find('i.mdi-numeric').should('exist')
     })
 
     it('Set the dynnamic properties: change input "icon" position', () => {
-        cy.get(buttonSetIcon).trigger('click')
-        cy.wait(100)
-        cy.get(buttonSetIconPosition).trigger('click')
-        cy.wait(100)
-        cy.get('.nrdb-ui-number-input.number-input-2 .v-field .v-field__append-inner i.mdi-numeric').should('exist')
+        cy.get('#nrdb-ui-widget-c7a86c799d34c234').should('exist')
+        cy.clickAndWait(cy.get('button').contains('Dynamic Property: Icon Position'))
+        cy.get('#nrdb-ui-widget-c7a86c799d34c234 .v-field .v-field__append-inner').find('i.mdi-numeric').should('exist')
     })
 
-    it('Set the dynnamic properties: move input "icon" to outside', () => {
-        cy.get(buttonSetIcon).trigger('click')
-        cy.wait(100)
-        cy.get(buttonSetIconPosition).trigger('click')
-        cy.wait(100)
-        cy.get(buttonSetIconOutside).trigger('click')
-        cy.wait(100)
-        cy.get('.v-input__prepend').find('i.mdi-numeric').should('exist')
+    it('Set the dynnamic properties: move input "icon" to', () => {
+        cy.get('#nrdb-ui-widget-df1f392297c5e2b1').should('exist')
+        cy.clickAndWait(cy.get('button').contains('Dynamic Property: Icon Inner Position'))
+        cy.get('#nrdb-ui-widget-df1f392297c5e2b1 .v-input .v-input__prepend').find('i.mdi-numeric').should('exist')
     })
 })
