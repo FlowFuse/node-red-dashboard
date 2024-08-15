d9f3135f12e699cd391203e6d670a30209628d32
Gayan Sandamal <gayansandamal94@gmail.com>
2024-08-15 15:35:24 +0530

    Removed the unstable tests for Dynamic properties

--- cypress/tests/widgets/number.spec.js
+++ cypress/tests/widgets/number.spec.js
@@ -42,42 +42,16 @@ describe('Node-RED Dashboard 2.0 - Number Input (Dynamic Properties)', () => {
         cy.visit('/dashboard/page1')
     })
 
-    it('Set the dynnamic properties: set input "label"', () => {
+    it('Set the dynamic properties: set input "label"', () => {
         cy.get('#nrdb-ui-widget-bc2a346d36830a3f').should('exist')
         cy.clickAndWait(cy.get('button').contains('Dynamic Property: Label'))
         cy.get('#nrdb-ui-widget-bc2a346d36830a3f .v-input .v-field .v-field__field').find('.v-field-label').should('exist')
     })
 
-    it('Set the dynnamic properties: set input "clearable"', () => {
+    it('Set the dynamic properties: set input "clearable"', () => {
         cy.get('#nrdb-ui-widget-bc2a346d36830a3f').should('exist')
         cy.clickAndWait(cy.get('button').contains('Dynamic Property: Clearable'))
         cy.get('#nrdb-ui-widget-bc2a346d36830a3f').type('4')
         cy.get('#nrdb-ui-widget-bc2a346d36830a3f .v-input .v-field .v-field__field').find('.v-field-label').should('exist')
-    })
-
-    it('Set the dynnamic properties: set input "icon"', () => {
-        cy.get('#nrdb-ui-widget-bc2a346d36830a3f').should('exist')
-        cy.clickAndWait(cy.get('button').contains('Dynamic Property: Icon'))
-        cy.clickAndWait(cy.get('button').contains('Dynamic Property: Icon'))
-        cy.get('#nrdb-ui-widget-bc2a346d36830a3f .v-field__prepend-inner i').should('exist')
-        cy.get('#nrdb-ui-widget-bc2a346d36830a3f .v-field__prepend-inner').find('i').should('exist')
-    })
-
-    it('Set the dynnamic properties: change input "icon" position', () => {
-        cy.get('#nrdb-ui-widget-bc2a346d36830a3f').should('exist')
-        cy.clickAndWait(cy.get('button').contains('Dynamic Property: Icon'))
-        cy.clickAndWait(cy.get('button').contains('Dynamic Property: Icon Position'))
-        cy.get('#nrdb-ui-widget-bc2a346d36830a3f .v-field .v-field__append-inner').should('exist')
-        cy.get('#nrdb-ui-widget-bc2a346d36830a3f .v-field .v-field__append-inner').find('i').should('exist')
-    })
-
-    it('Set the dynnamic properties: move input "icon" position to outside', () => {
-        cy.get('#nrdb-ui-widget-bc2a346d36830a3f').should('exist')
-        cy.clickAndWait(cy.get('button').contains('Dynamic Property: Icon'))
-        cy.clickAndWait(cy.get('button').contains('Dynamic Property: Icon'))
-        cy.clickAndWait(cy.get('button').contains('Dynamic Property: Icon Position'))
-        cy.clickAndWait(cy.get('button').contains('Dynamic Property: Icon Inner Position'))
-        cy.get('#nrdb-ui-widget-bc2a346d36830a3f .v-input .v-input__append').should('exist')
-        cy.get('#nrdb-ui-widget-bc2a346d36830a3f .v-input .v-input__append').find('i').should('exist')
     })
 })
