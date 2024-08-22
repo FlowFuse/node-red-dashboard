ed954837d66d3b86003bba8c7c7f98b64d467a87
Gayan Sandamal <gayansandamal94@gmail.com>
2024-08-22 13:41:57 +0530

    test descirption change

--- cypress/tests/widgets/radio-group.spec.js
+++ cypress/tests/widgets/radio-group.spec.js
@@ -9,8 +9,8 @@ describe('Node/-RED Dashboard 2.0 - Radio Group Widget', () => {
         cy.get('#nrdb-ui-widget-c39496d2b6c5b35f > .v-input > .v-input__control > .v-selection-control-group').should('exist')
     })
 
-    // Test case: Emits onClear event correctly
-    it('reset field on onClear event correctly and outputs the correct payload', () => {
+    // Test case: Emits option select event correctly
+    it('Select option correctly and outputs the correct payload', () => {
         cy.clickAndWait(cy.get('#nrdb-ui-widget-c39496d2b6c5b35f .v-selection-control:nth-child(1) > .v-label--clickable'))
         cy.get('#nrdb-ui-widget-c39496d2b6c5b35f .v-selection-control:nth-child(1) input[type="radio"]').should('have.value', 'option1')
         cy.checkOutput('msg.payload', 'option1')
