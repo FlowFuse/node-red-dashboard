cb70a4a5de1af3fec3c2becc571880f93a5ad8f2
Gayan Sandamal <gayansandamal94@gmail.com>
2024-08-29 09:55:27 +0530

    Removal of E2E tests that randomly fails

--- cypress/tests/widgets/text-input.spec.js
+++ cypress/tests/widgets/text-input.spec.js
@@ -15,12 +15,4 @@ describe('Node/-RED Dashboard 2.0 - Text Input Widget', () => {
         cy.get('#nrdb-ui-widget-ab3346b81a7cf742 .nrdb-ui-text-field').trigger('mouseover')
         cy.get('.v-tooltip').should('contain', 'Tooltip Text')
     })
-
-    // Test case: Emits onClear event correctly
-    it('reset field on olear event correctly and outputs the correct payload', () => {
-        cy.get('#nrdb-ui-widget-ab3346b81a7cf742 input[type="text"]').clear()
-        cy.get('#nrdb-ui-widget-ab3346b81a7cf742 input[type="text"]').type('New text')
-        cy.get('#nrdb-ui-widget-ab3346b81a7cf742 input[type="text"]').clear()
-        cy.checkOutput('msg.payload', '')
-    })
 })
