8b5ed3c7b00da417675ac42ab9fec86596e7b2b5
Gayan Sandamal <gayansandamal94@gmail.com>
2024-09-06 20:51:52 +0530

    Min max limintations, responsive UI imrpvements server side data storing support

--- nodes/widgets/ui_number_input.js
+++ nodes/widgets/ui_number_input.js
@@ -1,4 +1,6 @@
 const datastore = require('../store/data.js')
+const statestore = require('../store/state.js')
+const { appendTopic } = require('../utils/index.js')
 
 module.exports = function (RED) {
     function NumberInputNode (config) {
@@ -15,6 +17,33 @@ module.exports = function (RED) {
 
         const evts = {
             onChange: true,
+            beforeSend: async function (msg) {
+                const updates = msg.ui_update
+                if (updates) {
+                    if (typeof updates.label !== 'undefined') {
+                        // dynamically set "label" property
+                        statestore.set(group.getBase(), node, msg, 'label', updates.label)
+                    }
+                    if (typeof updates.clearable !== 'undefined') {
+                        // dynamically set "clearable" property
+                        statestore.set(group.getBase(), node, msg, 'clearable', updates.clearable)
+                    }
+                    if (typeof updates.icon !== 'undefined') {
+                        // dynamically set "icon" property
+                        statestore.set(group.getBase(), node, msg, 'icon', updates.icon)
+                    }
+                    if (typeof updates.iconPosition !== 'undefined') {
+                        // dynamically set "iconPosition" property
+                        statestore.set(group.getBase(), node, msg, 'iconPosition', updates.iconPosition)
+                    }
+                    if (typeof updates.iconInnerPosition !== 'undefined') {
+                        // dynamically set "iconInnerPosition" property
+                        statestore.set(group.getBase(), node, msg, 'iconInnerPosition', updates.iconInnerPosition)
+                    }
+                }
+                msg = await appendTopic(RED, config, node, msg)
+                return msg
+            },
             onInput: function (msg, send) {
                 // store the latest msg passed to node
                 datastore.save(group.getBase(), node, msg)
