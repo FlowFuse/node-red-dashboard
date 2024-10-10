9b642a2fe7553fc050e8f544252eecbd3e6c10d9
Gayan Sandamal <gayansandamal94@gmail.com>
2024-10-10 14:06:32 +0530

    Update ui/src/mixins/responsiveness.js
    Co-authored-by: Stephen McLaughlin <44235289+Steve-Mcl@users.noreply.github.com>

--- ui/src/mixins/responsiveness.js
+++ ui/src/mixins/responsiveness.js
@@ -26,12 +26,6 @@ export default {
     methods: {
         countColumns () {
             let cols = 0
-            const defaultBreakpoints = [
-                { name: 'Default', px: 0, cols: 3 },
-                { name: 'Tablet', px: 576, cols: 6 },
-                { name: 'Small Desktop', px: 768, cols: 9 },
-                { name: 'Desktop', px: 1024, cols: 12 }
-            ]
             if (this.page) {
                 // set default breakpoints if none are defined
                 const b = this.page.breakpoints || defaultBreakpoints
