99deb4808e32d88557cb49302661254a0cbfe9d6
Steve-Mcl <sdmclaughlin@gmail.com>
2024-10-31 11:00:13 +0000

    Ad ui-spacer node

---
+++ ui/src/widgets/ui-spacer/UISpacer.vue
@@ -0,0 +1,19 @@
+<template>
+    <v-spacer class="nrdb-spacer" />
+</template>
+
+<script>
+
+export default {
+    name: 'DBUISpacer',
+    inject: ['$socket', '$dataTracker'],
+    props: {
+        id: { type: String, required: true },
+        props: { type: Object, default: () => ({}) },
+        state: { type: Object, default: () => ({}) }
+    },
+    created () {
+        this.$socket.emit('widget-load', this.id)
+    }
+}
+</script>
