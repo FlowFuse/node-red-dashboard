b2d7e2adff60a1655eecb2e3f8ad995d7e108284
Joe Pavitt <joepavitt@flowforge.com>
2024-11-04 16:34:07 +0000

    Add edit-mode styling for the spacers

--- ui/src/widgets/ui-spacer/UISpacer.vue
+++ ui/src/widgets/ui-spacer/UISpacer.vue
@@ -17,3 +17,21 @@ export default {
     }
 }
 </script>
+
+<style>
+.nrdb-edit-mode .nrdb-spacer {
+    background-color: #007cff12;
+    border: 1px dashed #71aafc;
+    border-radius: 4px;
+}
+
+.nrdb-edit-mode .nrdb-spacer:before {
+    content: "spacer";
+    font-style: italic;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+    color: #0049ff75;
+}
+</style>
