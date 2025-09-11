39372b762a4b328cbb696acf9dc34c4fe320ba3a
Ben Hardill <ben@flowforge.com>
2025-09-11 15:15:40 +0100

    Ensure number widget shows decimal increments
    fixes #1697
    
    Adds `:precision="null" to the `v-number-input` to allow it to show
    as many decimal places as needed

--- ui/src/widgets/ui-number-input/UINumberInput.vue
+++ ui/src/widgets/ui-number-input/UINumberInput.vue
@@ -7,7 +7,7 @@
                     v-model="value" :class="{'compressed': isCompressed, 'stacked-spinner': spinner === 'stacked'}" :reverse="false" :controlVariant="spinner" :hideInput="false" :inset="false"
                     v-bind="props" :disabled="!state.enabled"
                     :rules="validation" :clearable="clearable" variant="outlined" hide-details="auto"
-                    :prepend-icon="prependIcon" :append-icon="appendIcon" :append-inner-icon="appendInnerIcon"
+                    :prepend-icon="prependIcon" :append-icon="appendIcon" :append-inner-icon="appendInnerIcon" :precision="null"
                     :prepend-inner-icon="prependInnerIcon" :min="min" :max="max" :step="step" @update:model-value="onChange" @keyup.enter="onEnter" @blur="onBlur" @click:clear="onClear"
                 >
                     <template #label>
