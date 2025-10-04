8f1b2608afebf4d6d2adace1ce4057548aa68d22
colinl <clanlaw@gmail.com>
2025-10-04 17:32:59 +0100

    Move const niceNumber = (x) => { definition to before it is used

--- ui/src/widgets/ui-chart/helpers/axis.helper.js
+++ ui/src/widgets/ui-chart/helpers/axis.helper.js
@@ -1,3 +1,24 @@
+// Round x to a nice number, using factors of 1, 2, 5 or 10
+// and determine how many decimal places are needed to show it
+const niceNumber = (x) => {
+    // find x as a number between 1 and 9.9999 with power of 10 multiplier
+    // eg, 68.7 is 6.87 * 10^1 so exp is 1 and f is 6.8
+    const exp = Math.floor(Math.log10(x))
+    const f = x / Math.pow(10, exp)
+
+    // round f to 1, 2 5 or 10
+    let niceFraction
+    if (f < 1.5) niceFraction = 1
+    else if (f < 3) niceFraction = 2
+    else if (f < 7) niceFraction = 5
+    else niceFraction = 10
+    // and scale it up to the range of the input value so if x were 68.7 this returns 50
+    const niceX = niceFraction * Math.pow(10, exp)
+    // determine the number of decimal places necessary to represent this
+    const decimals = Math.max(0, -exp)
+    return [niceX, decimals]
+}
+
 const getAxisMinMax = (value) => {
     const min = typeof value?.min === 'number' ? value?.min : 0
     let max = typeof value?.max === 'number' ? value?.max : 1
@@ -21,27 +42,6 @@ const getAxisMinMax = (value) => {
         min: axisMin,
         max: axisMax
     }
-}
-
-// Round x to a nice number, using factors of 1, 2, 5 or 10
-// and determine how may decimal places are needed to show it
-const niceNumber = (x) => {
-    // find x as a number between 1 and 9.9999 with power of 10 multiplier
-    // eg, 68.7 is 6.87 * 10^1 so exp is 1 and f is 6.8
-    const exp = Math.floor(Math.log10(x))
-    const f = x / Math.pow(10, exp)
-
-    // round f to 1, 2 5 or 10
-    let niceFraction
-    if (f < 1.5) niceFraction = 1
-    else if (f < 3) niceFraction = 2
-    else if (f < 7) niceFraction = 5
-    else niceFraction = 10
-    // and scale it up to the range of the input value so if x were 68.7 this returns 50
-    const niceX = niceFraction * Math.pow(10, exp)
-    // determine the number of decimal places necessary to represent this
-    const decimals = Math.max(0, -exp)
-    return [niceX, decimals]
 }
 
 const getAxisMin = (value) => {
