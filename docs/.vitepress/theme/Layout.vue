08bc1daa0a0bfb8a7364b5d4f7b2b26fb16f087d
Yndira-E <yndira@flowfuse.com>
2025-11-03 18:42:40 +0100

    Comment banner out

--- docs/.vitepress/theme/Layout.vue
+++ docs/.vitepress/theme/Layout.vue
@@ -1,5 +1,5 @@
 <script setup>
-import AdvertBanner from '../../components/AdvertBanner.vue';
+// import AdvertBanner from '../../components/AdvertBanner.vue';
 import DefaultTheme from 'vitepress/theme'
 
 const { Layout } = DefaultTheme
@@ -7,10 +7,10 @@ const { Layout } = DefaultTheme
 
 <template>
   <Layout>
-    <template #layout-top>
+    <!-- <template #layout-top>
       <AdvertBanner href="https://events.zoom.us/ev/AqhqiQ8mTK2lnAoOEH8c8TA1a_9MzVhZq_T7d1-kMHlHDt2_Qh_0~Au3w7qmqpnpiahQlhdbsX2WTIU1irW402wkglZXvNQ12Ip_8m4ToMu4vQ5fp7ujvOn7_f6weZ70nr35LYHAmA1--vg">
         <b>Node-RED Con 2025 – Nov. 4:</b> Global Community Conference on Industrial Applications — <span class="underline">Register free today!</span>
       </AdvertBanner>
-    </template>
+    </template> -->
   </Layout>
 </template>
